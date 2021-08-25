a795c2283492ef2c0d375b79faba3b1e9158280e
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2021-08-25 13:18:21 -0300

    Calculadora Basica em C# exe e cs

---
+++ calculadora/Calculadora/Calculadora/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblV1 = new System.Windows.Forms.Label();
+            this.lblV2 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.lblTtl = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblV1
+            // 
+            this.lblV1.AutoSize = true;
+            this.lblV1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblV1.Location = new System.Drawing.Point(12, 92);
+            this.lblV1.Name = "lblV1";
+            this.lblV1.Size = new System.Drawing.Size(77, 28);
+            this.lblV1.TabIndex = 0;
+            this.lblV1.Text = "Valor 1:";
+            this.lblV1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblV2
+            // 
+            this.lblV2.AutoSize = true;
+            this.lblV2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblV2.Location = new System.Drawing.Point(12, 141);
+            this.lblV2.Name = "lblV2";
+            this.lblV2.Size = new System.Drawing.Size(77, 28);
+            this.lblV2.TabIndex = 0;
+            this.lblV2.Text = "Valor 2:";
+            this.lblV2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(95, 97);
+            this.txt1.MaxLength = 4;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(162, 23);
+            this.txt1.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(95, 146);
+            this.txt2.MaxLength = 4;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(162, 23);
+            this.txt2.TabIndex = 2;
+            this.txt2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // lblTtl
+            // 
+            this.lblTtl.AutoSize = true;
+            this.lblTtl.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTtl.Location = new System.Drawing.Point(22, 40);
+            this.lblTtl.Name = "lblTtl";
+            this.lblTtl.Size = new System.Drawing.Size(58, 28);
+            this.lblTtl.TabIndex = 0;
+            this.lblTtl.Text = "Total:";
+            this.lblTtl.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtResultado.Location = new System.Drawing.Point(95, 37);
+            this.txtResultado.MaxLength = 4;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(162, 34);
+            this.txtResultado.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(58, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(160, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(58, 289);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 41);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(160, 289);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 41);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(57, 343);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(192, 40);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Limpar Campos!";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTtl);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lblV2);
+            this.Controls.Add(this.lblV1);
+            this.Name = "Form1";
+            this.Text = "Calculadora - Basico";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblV1;
+        private System.Windows.Forms.Label lblV2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label lblTtl;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ calculadora/Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        /*
+            if (txt1.Text == null || txt2.Text == null )
+            {
+                MessageBox.Show("Os campos estão vazios!", "Calculadora", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        */
+
+            float valor1, valor2, resultado;
+
+            valor1 = float.Parse(txt1.Text);
+            valor2 = float.Parse(txt2.Text);
+
+
+            resultado = valor1 + valor2;
+
+            txtResultado.Text = resultado.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float valor1, valor2, resultado;
+
+            valor1 = float.Parse(txt1.Text);
+            valor2 = float.Parse(txt2.Text);
+
+            resultado = valor1 - valor2;
+
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float valor1, valor2, resultado;
+
+            valor1 = float.Parse(txt1.Text);
+            valor2 = float.Parse(txt2.Text);
+
+            resultado = valor1 * valor2;
+
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float valor1, valor2, resultado;
+
+            valor1 = float.Parse(txt1.Text);
+            valor2 = float.Parse(txt2.Text);
+
+            resultado = valor1 / valor2;
+
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txt1.Clear();
+            txt2.Clear();
+            txtResultado.Clear();
+            txt1.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
