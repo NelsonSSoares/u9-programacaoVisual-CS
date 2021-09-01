78e7bb8cb1005d787c2ea5b6a2342de97d2aecc1
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2021-09-01 11:17:28 -0300

    Add files via upload

---
+++ Fliperama/Fliperama desc Ficha/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace Fliperama_desc_Ficha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtQtd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Qtd Fichas:";
+            // 
+            // txtQtd
+            // 
+            this.txtQtd.Location = new System.Drawing.Point(15, 39);
+            this.txtQtd.Name = "txtQtd";
+            this.txtQtd.Size = new System.Drawing.Size(82, 23);
+            this.txtQtd.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Total a Pagar:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(15, 98);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 23);
+            this.txtTotal.TabIndex = 3;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(212, 30);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(133, 39);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(212, 82);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(133, 39);
+            this.btnClean.TabIndex = 5;
+            this.btnClean.Text = "&Limpar";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 169);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtQtd);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Desconto Ficha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQtd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClean;
+    }
+}
+
---
+++ Fliperama/Fliperama desc Ficha/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fliperama_desc_Ficha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            float fichas, total;
+            fichas = float.Parse(txtQtd.Text);
+            if ( fichas >= 10) 
+            {
+                total = fichas * 5;
+            }else
+            {
+                total = fichas * 6;
+            }
+            txtTotal.Text = total.ToString(); 
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente limpar?", "Limpar Campos", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK) { 
+                txtQtd.Clear();
+                txtTotal.Clear();
+                txtQtd.Focus();
+            }
+        }
+    }
+}
