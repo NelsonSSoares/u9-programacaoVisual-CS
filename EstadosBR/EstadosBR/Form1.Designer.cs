6e494ab106a039384095844dc8497fa17886be93
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2021-09-29 11:36:22 -0300

    show state flag

---
+++ EstadosBR/EstadosBR/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace EstadosBR
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboEstado = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picEstado = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picEstado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboEstado
+            // 
+            this.cboEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEstado.FormattingEnabled = true;
+            this.cboEstado.Items.AddRange(new object[] {
+            "AC",
+            "AL",
+            "AP",
+            "AM",
+            "BA",
+            "CE",
+            "DF",
+            "ES",
+            "GO",
+            "MA",
+            "MT",
+            "MS",
+            "MG",
+            "PA",
+            "PB",
+            "PR",
+            "PE",
+            "PI",
+            "RJ",
+            "RN",
+            "RS",
+            "RO",
+            "RR",
+            "SC",
+            "SP",
+            "SE",
+            "TO"});
+            this.cboEstado.Location = new System.Drawing.Point(105, 31);
+            this.cboEstado.Name = "cboEstado";
+            this.cboEstado.Size = new System.Drawing.Size(121, 23);
+            this.cboEstado.TabIndex = 0;
+            this.cboEstado.SelectedIndexChanged += new System.EventHandler(this.cboEstado_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Estado:";
+            // 
+            // picEstado
+            // 
+            this.picEstado.Location = new System.Drawing.Point(33, 60);
+            this.picEstado.Name = "picEstado";
+            this.picEstado.Size = new System.Drawing.Size(288, 420);
+            this.picEstado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picEstado.TabIndex = 2;
+            this.picEstado.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 492);
+            this.Controls.Add(this.picEstado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboEstado);
+            this.Name = "Form1";
+            this.Text = "Estados BR";
+            ((System.ComponentModel.ISupportInitialize)(this.picEstado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboEstado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picEstado;
+    }
+}
+
---
+++ EstadosBR/EstadosBR/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstadosBR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cboEstado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboEstado.Text == "AC")
+            {
+                picEstado.Load("acre.jpg");
+            }
+            else if (cboEstado.Text == "AL")
+            {
+                picEstado.Load("alagoas.jpg");
+            }
+            else if (cboEstado.Text == "AP")
+            {
+                picEstado.Load("amapa.jpg");
+            }
+            else if (cboEstado.Text == "AM")
+            {
+                picEstado.Load("amazonas.jpg");
+            }
+            else if (cboEstado.Text == "BA")
+            {
+                picEstado.Load("bahia.jpg");
+            }
+            else if (cboEstado.Text == "CE")
+            {
+                picEstado.Load("ceara.jpg");
+            }
+            else if (cboEstado.Text == "DF")
+            {
+                picEstado.Load("brasilia.jpg");
+            }
+            else if (cboEstado.Text == "ES")
+            {
+                picEstado.Load("espirito-santo.jpg");
+            }
+            else if (cboEstado.Text == "GO")
+            {
+                picEstado.Load("goias.jpg");
+            }
+            else if (cboEstado.Text == "MA")
+            {
+                picEstado.Load("maranhao.jpg");
+            }
+            else if (cboEstado.Text == "MT")
+            {
+                picEstado.Load("mato-grosso.jpg");
+            }
+            else if (cboEstado.Text == "MS")
+            {
+                picEstado.Load("mato-grosso-do-sul.jpg");
+            }
+            else if (cboEstado.Text == "MG")
+            {
+                picEstado.Load("minas-gerais.jpg");
+            }
+            else if (cboEstado.Text == "PA")
+            {
+                picEstado.Load("para.jpg");
+            }
+            else if (cboEstado.Text == "PB")
+            {
+                picEstado.Load("paraiba.jpg");
+            }
+            else if (cboEstado.Text == "PR")
+            {
+                picEstado.Load("parana.jpg");
+            }
+            else if (cboEstado.Text == "PE")
+            {
+                picEstado.Load("pernambuco.jpg");
+            }
+            else if (cboEstado.Text == "PI")
+            {
+                picEstado.Load("piaui.jpg");
+            }
+            else if (cboEstado.Text == "RJ")
+            {
+                picEstado.Load("rio-de-janeiro.jpg");
+            }
+            else if (cboEstado.Text == "RN")
+            {
+                picEstado.Load("rio-grande-do-norte.jpg");
+            }
+            else if (cboEstado.Text == "RS")
+            {
+                picEstado.Load("rio-grande-do-sul.jpg");
+            }
+            else if (cboEstado.Text == "RO")
+            {
+                picEstado.Load("rondonia.jpg");
+            }
+            else if (cboEstado.Text == "RR")
+            {
+                picEstado.Load("roraima.jpg");
+            }
+            else if (cboEstado.Text == "SC")
+            {
+                picEstado.Load("santa-catarina.jpg");
+            }
+            else if (cboEstado.Text == "SP")
+            {
+                picEstado.Load("sao-paulo.jpg");
+            }
+            else if (cboEstado.Text == "SE")
+            {
+                picEstado.Load("sergipe.jpg");
+            }
+            else if (cboEstado.Text == "TO")
+            {
+                picEstado.Load("tocantins.jpg");
+            }
+        }
+    }
+}
