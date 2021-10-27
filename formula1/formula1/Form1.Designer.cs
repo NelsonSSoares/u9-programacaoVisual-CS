79d498e96feab32fa16df50c07b8c91e9d671610
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2021-10-27 12:22:25 -0300

    carrega os 7 primeiros pilotos da ultima corrida

---
+++ formula1/formula1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace formula1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboClass = new System.Windows.Forms.ComboBox();
+            this.txtnumPilot = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picPilot = new System.Windows.Forms.PictureBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picPilot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cboClass
+            // 
+            this.cboClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboClass.FormattingEnabled = true;
+            resources.ApplyResources(this.cboClass, "cboClass");
+            this.cboClass.Name = "cboClass";
+            this.cboClass.SelectedIndexChanged += new System.EventHandler(this.cboClass_SelectedIndexChanged);
+            // 
+            // txtnumPilot
+            // 
+            resources.ApplyResources(this.txtnumPilot, "txtnumPilot");
+            this.txtnumPilot.Name = "txtnumPilot";
+            this.txtnumPilot.TextChanged += new System.EventHandler(this.txtnumPilot_TextChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // picPilot
+            // 
+            resources.ApplyResources(this.picPilot, "picPilot");
+            this.picPilot.Name = "picPilot";
+            this.picPilot.TabStop = false;
+            this.picPilot.Click += new System.EventHandler(this.picPilot_Click);
+            // 
+            // btnSearch
+            // 
+            resources.ApplyResources(this.btnSearch, "btnSearch");
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.picPilot);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtnumPilot);
+            this.Controls.Add(this.cboClass);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPilot)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboClass;
+        private System.Windows.Forms.TextBox txtnumPilot;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picPilot;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ formula1/formula1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formula1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picPilot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int posit = int.Parse(cboClass.Text);
+
+            switch (posit)
+            {
+                case 1:
+                    picPilot.Load("max-verstappe.jpg");
+                    break;
+                case 2:
+                    picPilot.Load("Lewis_Hamilton.jpg");
+                    break;
+                case 3:
+                    picPilot.Load("bottas.jpg");
+                    break;
+                case 4:
+                    picPilot.Load("sergio-perez.jpg");
+                    break;
+                case 5:
+                    picPilot.Load("lando-norris.jpg");
+                    break;
+                case 6:
+                    picPilot.Load("Charles-Leclerc.jpg");
+                    break;
+                case 7:
+                    picPilot.Load("Carlos-Sainz.jpg");
+                    break;
+            }
+
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            cboClass.Items.Clear();
+
+            int nump = 0 , i;
+
+            if (string.IsNullOrEmpty(txtnumPilot.Text))
+            {
+                MessageBox.Show("O Campo deve ser Preenchido ", "Formula 1", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtnumPilot.Focus();
+
+            }
+            else {
+                nump = int.Parse(txtnumPilot.Text);
+            }
+    
+
+            if (nump > 7 || nump <= 0 || nump == null )
+            {
+                MessageBox.Show("a Quantidade deve ser entre 1 e 7 ", "Formula 1", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtnumPilot.Focus();
+            }
+
+            for (i = 0; i <= nump; i++)
+            {
+                cboClass.Items.Add(i);
+            }
+
+        }
+
+        private void txtnumPilot_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
